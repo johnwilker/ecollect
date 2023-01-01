8c56561c7673c2a64b05f453b0a373b19ae68577
John Wilker <johnwilker2@gmail.com>
2023-01-01 13:41:23 -0300

    added search results page and search modal

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,12 +24,15 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+  citySelect.innerHTML = "<option value>Selecione a cidade</option>";
+  citySelect.disabled - true;
+
   fetch(url)
   // .then( (res) => {return res.json })
   .then( res => res.json() )
   .then( cities => {
     for ( const city of cities ) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
 
     citySelect.disabled = false;
@@ -39,3 +42,47 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+
+// Collect items
+// Select all li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for ( const item of itemsToCollect ) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+  // add or remove class with javascript
+  itemLi.classList.toggle("selected");
+
+  const itemId = event.target.dataset.id
+
+  //verify for selected items and take the selected items
+  
+  const alreadySelected = selectedItems.findIndex( item => {
+    const itemFound = item === itemId; // it's going to be true or false
+    return itemFound;
+  })
+
+  // if is selected remove selected item
+  if( alreadySelected >= 0 ) {
+    // remove selected item
+    const filteredItems = selectedItems.filter( item => {
+      const itemIsDiferent = item != itemId; // false
+      return itemIsDiferent;
+    })
+    selectedItems = filteredItems;
+  } else {
+    // if is not selected add selected item
+    selectedItems.push( itemId );
+  }
+  // update hidden input with selected item
+  collectedItems.value = selectedItems;
+}
